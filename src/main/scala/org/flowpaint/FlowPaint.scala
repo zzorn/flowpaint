356a14a3b0198d780637784edc9184daf5b7e077
Hans Häggström <zzorn@iki.fi>
2008-11-13 18:10:40 +0000

    Maven POM file, an empty main method, and some example tests.

---
+++ src/main/scala/org/flowpaint/FlowPaint.scala
@@ -0,0 +1,15 @@
+package org.flowpaint
+
+/**
+ * Main entrypoint for FlowPaint.
+ *
+ */
+object FlowPaint {
+
+  def main(args: Array[String]) = {
+    
+    println( "FlowPaint started." )
+
+  }
+
+}

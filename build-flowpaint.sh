6de16a8971f545b8954058b4efad9fd09b3d4d63
Hans Häggström <zzorn@iki.fi>
2008-11-19 11:41:11 +0000

    spelling

--- build-flowpaint.sh
+++ build-flowpaint.sh
@@ -3,5 +3,5 @@ echo "#### Building flowpaint executable jar"
 mvn clean
 mvn assembly:assembly
 
-echo "#### Building done.  Assuming there were no errors, you can run FLowPaint with ./flowpaint.sh"
+echo "#### Building done.  Assuming there were no errors, you can run FlowPaint with ./flowpaint.sh"
 

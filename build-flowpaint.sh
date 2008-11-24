4b3c83e744dbeec46438b11c20bc486fab5dde3b
Hans Häggström <zzorn@iki.fi>
2008-11-24 21:59:52 +0000

    Fixed source build file to move the compiled jar to the place where the new flowpaint.sh expects it to be.

--- build-flowpaint.sh
+++ build-flowpaint.sh
@@ -3,5 +3,8 @@ echo "#### Building flowpaint executable jar"
 mvn clean
 mvn assembly:assembly
 
+echo "#### Moving and renaming produced jar"
+cp target/flowpaint-executable.jar flowpaint.jar
+
 echo "#### Building done.  Assuming there were no errors, you can run FlowPaint with ./flowpaint.sh"
 

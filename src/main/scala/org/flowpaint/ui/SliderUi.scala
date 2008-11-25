d29f1e927044831ed502367e60247794adf2dae2
Hans Häggström <zzorn@iki.fi>
2008-11-25 16:18:09 +0000

    working on slider ui

--- src/main/scala/org/flowpaint/ui/SliderUi.scala
+++ src/main/scala/org/flowpaint/ui/SliderUi.scala
@@ -2,6 +2,8 @@ package org.flowpaint.ui
 
 import _root_.org.flowpaint.brush.Brush
 import _root_.org.flowpaint.util.DataSample
+import java.awt.Graphics
+import renderer.SingleRenderSurface
 
 /**
  * 
@@ -16,5 +18,12 @@ class SliderUi( editedData : DataSample,
                 previewBrush : => Brush ) extends ParameterUi(editedData) {
 
   // TODO: Slider implementation, with a brush used to render the preview
-  
+
+  // TODO: Reuse BrushPreview
+
+  val surface = new SingleRenderSurface()
+  val paintPanel = new PaintPanel( surface, false )
+
+//  previewBrush().
+
 }

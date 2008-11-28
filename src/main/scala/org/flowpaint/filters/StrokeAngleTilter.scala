5e3dfef7287b95e47e5b542e5b6189583cbb467c
Hans Häggström <zzorn@iki.fi>
2008-11-28 16:32:22 +0000

    Now the stroke angle tilting filter stores the first point of a stroke, and re-sends it when it gets the second point, so that it can calculate the correct starting angle for the stroke.

--- src/main/scala/org/flowpaint/filters/StrokeAngleTilter.scala
+++ src/main/scala/org/flowpaint/filters/StrokeAngleTilter.scala
@@ -20,7 +20,11 @@ class StrokeAngleTilter( tilt:Float ) extends StrokeFilter {
   private var previousAngle = 0f
   private val smooth = 0.8f
 
+  private var firstPoint :DataSample = null
+
   protected def filterStrokePoint(pointData: DataSample, resultCallback: (DataSample) => Unit) {
+
+    val first = pointData.getProperty("index", 2) < 1
 
     val newX = pointData.getProperty("x", previousX)
     val newY = pointData.getProperty("y", previousY)
@@ -31,7 +35,9 @@ class StrokeAngleTilter( tilt:Float ) extends StrokeFilter {
     val angle = HALF_Pi + Math.atan2(yDiff , xDiff).toFloat
 
     val normalizedAngle = util.MathUtils.normalizeAngle( angle )
-    val smoothedAngle = util.MathUtils.wrappedInterpolate( smooth, normalizedAngle, previousAngle )
+
+    val smoothing = if (first || firstPoint != null) 0f else smooth
+    val smoothedAngle = util.MathUtils.wrappedInterpolate( smoothing, normalizedAngle, previousAngle )
     previousAngle = smoothedAngle
 
 /*
@@ -46,7 +52,22 @@ class StrokeAngleTilter( tilt:Float ) extends StrokeFilter {
 */
     pointData.setProperty("angle", smoothedAngle * 2f * Math.Pi.toFloat )
 
-    resultCallback(pointData)
+    if ( first ){
+      // Only store the first point, don't send it forward yet
+      firstPoint = new DataSample()
+      firstPoint.setValuesFrom( pointData )
+    }
+    else {
+      if (firstPoint != null){
+        // Forward the stored first point, using the current angle
+        firstPoint.setProperty("angle", smoothedAngle * 2f * Math.Pi.toFloat )
+        resultCallback(firstPoint)
+        firstPoint = null
+      }
+
+      resultCallback(pointData)
+    }
+
   }
 
 

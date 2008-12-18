efa720ea6268ebcf858edfd284e762cfb5831fe6
Hans Häggström <zzorn@iki.fi>
2008-12-18 23:17:41 +0000

    Added settings for the shape.

--- src/main/scala/org/flowpaint/util/geospatial/shape/Shape.scala
+++ src/main/scala/org/flowpaint/util/geospatial/shape/Shape.scala
@@ -1,10 +1,17 @@
 package org.flowpaint.util.geospatial
+import property.Data
+
 /**
  * A concrete shape of some kind (instantiated geometry).
  * 
  * @author Hans Haggstrom
  */
 trait Shape {
+
+  /**
+   * Common settings for this Shape.
+   */
+  def settings : Data
 
   // TODO: Add the pixel processors to use for this shape, and maybe common settings / data
 

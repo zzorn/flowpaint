ca6546233a77ef4818b6adc1090e4fbf83e92a63
Hans Häggström <zzorn@iki.fi>
2009-01-24 14:45:30 +0000

    Added labels to slider controls.

--- src/main/scala/org/flowpaint/ui/editors/VolumeSliderEditor.scala
+++ src/main/scala/org/flowpaint/ui/editors/VolumeSliderEditor.scala
@@ -1,5 +1,6 @@
 package org.flowpaint.ui.editors
-import java.awt.{Graphics2D, Color}
+import java.awt.{Graphics2D, BorderLayout, Color}
+import javax.swing.{SwingConstants, JLabel}
 import util.{MathUtils}
 import util.GraphicsUtils._
 /**
@@ -9,21 +10,39 @@ import util.GraphicsUtils._
  */
 class VolumeSliderEditor extends SliderEditor {
 
-  protected def paintBackground(g2: Graphics2D, width: Int, height: Int) {
+  private val label = new JLabel()
 
+
+    override def onEditorCreated() =  {
+        super.onEditorCreated()
+
+        view.setLayout(new BorderLayout())
+        view.add( label, BorderLayout.CENTER )
+
+        label.setText( getStringProperty( "title", "" ) )
+        label.setForeground( Color.BLACK )
+        label.setHorizontalAlignment( SwingConstants.CENTER )
+    }
+
+    protected def paintBackground(g2: Graphics2D, width: Int, height: Int) {
+
+    val bg = 0.9f
+    g2.setColor( new Color( bg, bg, bg ) )
+    g2.fillRect( 0, 0, width, height )
+        
     val r = MathUtils.clampToZeroToOne( getFloatProperty( "red", 0 ) )
     val g = MathUtils.clampToZeroToOne( getFloatProperty( "green", 0 ) )
     val b = MathUtils.clampToZeroToOne( getFloatProperty( "blue", 0 ) )
-
-    g2.setColor( Color.WHITE )
-    g2.fillRect( 0, 0, width, height )
+    val a = 0.25f
 
 
     antialiased(g2) {
 
-      triangle( g2,  new Color( r, g, b ), 0, height / 2, width, 0, width, height )
+      triangle( g2,  new Color( r, g, b, a ), 0, height / 2, width, 0, width, height )
 
     }
+
+      
   }
 
 
